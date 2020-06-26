33dc9d8854e281e2548b38431b12a104a43adcd5
Hassan Bouzlima <bouzlima.h@live.fr>
2020-06-26 20:41:19 +0100

    commit

--- main.js
+++ main.js
@@ -3,16 +3,46 @@ var button = document.querySelector("button");
 
 
 
-button.addEventListener("click",function(){
+button.addEventListener("click", function () {
 
     var input = document.querySelector("input").value;
-    pushToDom(input);
+    callApi(input)
+
+})
+
+document.querySelector("input").addEventListener("keyup", function (e) {
+    var input = document.querySelector("input").value;
+    if (e.which === 13) { //enter key code is 13
+        callApi(input)
+
+    }
 
 
 })
 
-function pushToDom(input)
-{
-    document.getElementsByClassName("js-container")[0].innerHTML=input;
+function callApi(input) {
+    var url = "http://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=R73JSDFTivlxXOAkYeaHGfRzuowmSL9j";
+    var GiphyAJAXCall = new XMLHttpRequest();
+    GiphyAJAXCall.open('GET', url);
+    GiphyAJAXCall.send();
+    GiphyAJAXCall.addEventListener('load', function (e) {
+        var data = e.target.response;
+        pushToDom(data);
+
+    });
+}
+
+function pushToDom(input) {
+    var response = JSON.parse(input);
+    var imageUrls = response.data;
+    var container = document.querySelector(".js-container");
+    container.innerHTML = ""
+
+    imageUrls.forEach(function (image) {
+
+        container.innerHTML += "<img src=\"" + image.url + "\"class=\"container-image\">";
+
+    })
+
 
 }
